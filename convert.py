2a6292d24d77ffa48de71b7c8a8ca31b4506c734
bobsmith-seattleacademy <45773268+bobsmith-seattleacademy@users.noreply.github.com>
2021-11-07 14:06:44 -0800

    changed some stuff

--- convert.py
+++ convert.py
@@ -2,7 +2,11 @@ f = open("my.txt", "r")
 h = f.read().split()
 f.close()
 
+def myround(x, base=20):
+    return base * round(x/base)
+
 mydict = {}
+
 
 for line in h:
     (x,y) = line.split(",")
@@ -15,14 +19,42 @@ for line in h:
         i = mydict[y]
         mydict.update({y:(i[0],i[1]+1)})
 
+newdict = {}
+highest = None
+lowest = None
+for key in mydict:
+    if lowest == None:
+        lowest = int(key)
+    if highest == None:
+        highest = int(key)
+    if int(key) > highest:
+        highest = int(key)
+    if int(key) < lowest:
+        lowest = int(key)
+
+for i in range(myround(lowest)-20,myround(highest)+20+1,20):
+    newdict.update({i:(0,0)})
+
+
 
 for key in mydict:
-    i = mydict[key]
-    mydict.update({key:(i[0]/i[1])})
-print(mydict)
+
+    i = newdict[int(myround(float(key),20))]
+    newdict.update({myround(float(key),20):(i[0]+mydict[key][0],i[1]+mydict[key][1])})
+print(newdict)
+input()
+
+for key in newdict:
+    i = newdict[key]
+    try:
+        newdict.update({key:(i[0]/i[1])})
+    except ZeroDivisionError:
+        newdict.update({key:0})
+    
+print(newdict)
 
 f = open("new.txt","w")
-for key in mydict:
-    f.write("{},{}\n".format(key,mydict[key]))
+for key in newdict:
+    f.write("{},{}\n".format(key,newdict[key]))
     
     
